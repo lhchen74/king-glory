e891d1ddcf1ea426ebe7c92172f99a1d1346dbaa
Babb Chen <11435@SDC.SERCOMM.com>
2019-07-31 13:25:13 +0800

    admin add element and router

---
+++ admin/src/views/CategoryEdit.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>新建分类</h1>
+    <el-form>
+      <el-form-item label="名称" @submit.native.prevent="save">
+        <el-input v-model="model.name"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">保存</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      model: {}
+    };
+  },
+  methods: {
+    save() {
+      //   this.$http.post();
+    }
+  }
+};
+</script>
