5d2793e6f4958a2ce60d6a7aad67003669edc8af
Babb Chen <11435@SDC.SERCOMM.com>
2019-08-06 08:38:17 +0800

    Ad Add

--- admin/src/router.js
+++ admin/src/router.js
@@ -8,9 +8,11 @@ import ItemEdit from './views/ItemEdit.vue'
 import ItemList from './views/ItemList.vue'
 import HeroEdit from './views/HeroEdit.vue'
 import HeroList from './views/HeroList.vue'
-
 import ArticleEdit from './views/ArticleEdit.vue'
 import ArticleList from './views/ArticleList.vue'
+import AdEdit from './views/AdEdit.vue'
+import AdList from './views/AdList.vue'
+
 
 Vue.use(Router)
 
@@ -64,6 +66,18 @@ export default new Router({
       }, {
         path: '/articles/edit/:id',
         component: ArticleEdit,
+        props: true
+      },
+
+      {
+        path: '/ads/create',
+        component: AdEdit
+      }, {
+        path: '/ads/list',
+        component: AdList
+      }, {
+        path: '/ads/edit/:id',
+        component: AdEdit,
         props: true
       },
     ]
