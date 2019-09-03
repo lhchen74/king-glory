3c63bc1b01c4a45986834a93f0fdc3edf5c3d27a
Babb Chen <11435@SDC.SERCOMM.com>
2019-09-03 10:33:31 +0800

    npm build

--- admin/src/http.js
+++ admin/src/http.js
@@ -5,7 +5,8 @@ import router from './router'
 
 
 const http = axios.create({
-    baseURL: 'http://localhost:3000/admin/api/'
+    baseURL: process.env.VUE_APP_API_URL || '/admin/api'
+    // baseURL: 'http://localhost:3000/admin/api/'
 })
 
 // 全局请求拦截，添加验证 Headers
--- server/index.js
+++ server/index.js
@@ -11,6 +11,8 @@ require('./plugins/db')(app)
 require('./routes/admin')(app)
 require('./routes/web')(app)
 app.use('/uploads', express.static(__dirname + '/uploads'))
+app.use('/admin', express.static(__dirname + '/admin'))
+app.use('/', express.static(__dirname + '/web'))
 
 
 app.listen(3000, () => {
